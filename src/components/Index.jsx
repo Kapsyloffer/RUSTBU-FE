cc8f64173cd82fdc6de06abc9c5670559525e61d
Crilluz <CrilleMega@gmail.com>
2024-02-05 21:02:57 +0100

    Pimped out the landing page a bit

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,8 +1,12 @@
 import new_game from "./Calls/new_game";
+import rustbu from "./../img/banner.png";
+import './../App.css';
   
 const Index = () => {
     return ( 
-    <div className="Index">
+    <div class="Index">
+      <img src={rustbu} alt="logo"></img>
+      <br/>
     <button onClick={new_game}>New Game</button>
   </div>
     );
